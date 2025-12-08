8bc32381646e03781b195c4e21f9e3133d3f0512
halasyk_stanislav <galasik_stanislav@icloud.com>
2025-12-08 15:52:05 +0200

    restart HW

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,13 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Halasyk Stanislav</p>
           <p>
-            Contact us:{" "}
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            Contact us:
+            <a href="mailto:halasykstanislav@gmail.com">
+              {" "}
+              halasykstanislav@gmail.com
+            </a>
           </p>
         </div>
       </div>
